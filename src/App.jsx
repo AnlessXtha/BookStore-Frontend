4b26571528b98018ad7831d938fa4e56f5a2d037
AnlessXtha <aadesh.shrrstha@gmail.com>
2025-05-03 23:26:39 +0545

    completed the catalogue feature with proper API integration, cart and whitelist are also operational

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import { CartPage } from "./pages/CartPage";
 import Login from "./pages/Login";
-import Register from './pages/Register';
+import Register from "./pages/Register";
 import { Whitelist } from "./pages/Whitelist";
 import { Toaster } from "react-hot-toast";
-import Navbar from "./components/Navbar";
 import MainLayout from "./components/MainLayout";
-// import CataloguePage from "./pages/CataloguePage"; // Create this page too
+import BookCatalog from "./pages/BookCatalog";
+import BookDetails from "./pages/BookDetails";
 
 function App() {
   return (
     <>
       <Router>
-        <Toaster
-          position="top-center"
-          reverseOrder={false}
-        />
+        <Toaster position="top-center" reverseOrder={false} />
         <Routes>
           <Route element={<MainLayout />}>
             <Route path="/" element={<HomePage />} />
             <Route path="/cart" element={<CartPage />} />
             <Route path="/whitelist" element={<Whitelist />} />
-            {/* <Route path="/catalogue" element={<CataloguePage />} /> */}
-            {/* <Route path="/contact" element={<ContactPage />} /> */}
+            <Route path="/catalogue" element={<BookCatalog />} />
+            <Route path="/bookdetails/:id" element={<BookDetails />} />
           </Route>
 
           {/* Routes without Navbar */}
